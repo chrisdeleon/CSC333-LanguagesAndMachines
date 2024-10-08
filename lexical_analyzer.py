9fe56f5d0d9b9d399576f4b7e2cd39cf649b4ff2
chrisdeleon <deleonc2050@gmail.com>
2024-10-08 17:09:39 -0500

    added regular expressions for requirements

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -14,10 +14,47 @@ class Token:
         return f"Token({self.type}, {self.value})"
     
 
+def tokenize(input):
+    tokens = [] #token list
 
+    token_types = [
+        ('Integer', r'[0-9]*'),
+        ('Float', r'[0-9]+.[0-9]?[0-9]?[0-9]?'),
+        ('Addition', r'\+'),
+        ('Subtraction', r'-'),
+        ('Division', r'\/'),
+        ('Multiplication', r'\*'),
+        ('Power', r'\*\*'),
+        ('Modulo', r'%'),
+        ('Less Than', r'>'),
+        ('Greater Than', r'<'),
+        ('Not Equal', r'!='),
+        ('Equal', r'=='),
+        ('Greater Equal', r'>='),
+        ('Less Equal', r'<='),
+        ('Assignment', r'='),
+        ('And', r'&'),
+        ('Or', r'\|'),
+        ('Not', r'!'),
+        ('Left Parenthesis', r'\('),
+        ('Right Parenthesis', r'\)'),
+        ('Left Curly Bracket', r'{'),
+        ('Right Curly Bracket', r'}'),
+        ('Semi Colon', r';'),
+        ('Comma', r','),
+        ('Identifier', r'[A-Za-z_][A-Za-z0-9_]*'),
+        ('If', r'if'),
+        ('Else', r'else'),
+        ('While', r'while'),
+        ('Break', r'break'),
+        ('Read', r'read'),
+        ('Write', r'write'),
+        ('Function', r'function'),
+        ('Return', r'return'),
+        ('Comment', r'#.*')
+    ]
 
 # replace "output" with file location
 output_file = open('C:\\Users\\deleo\\OneDrive\\Desktop\\CSC333 HW2\\output.txt', 'w')
 #output_file.write("This writes into the file")
 #output_file.close()
-
